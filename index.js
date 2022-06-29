b3020bd87e4bb78c5f40f7951606e7775e5606f7
raymer belisario <raymer1986@gmail.com>
2022-06-29 18:09:54 -0400

    Menu ccs y validaciones de usuarios a pruebas

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 import { LoadGame } from "./config/initialScreen.js";
+import { validateName, confirmed } from "./models.js/users.js";
+import { mainMenu, Menu } from "./models.js/menu.js";
+
+async function initGame(){
+    //pantalla de carga del juego
+        await LoadGame
+    //validacion y confirmacion del usuario
+       
+}
+        initGame()
+        validateName()
+        
+        //Menú y Selección de opciones     
+        mainMenu.showMenu()
+        mainMenu.selectOpt()
+
+
+
+
+
+
